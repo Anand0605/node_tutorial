c58f8f94170a8eac4ab00994cd1d606528164167
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-10-22 00:29:58 +0530

    update

---
+++ server.js
@@ -0,0 +1,26 @@
+// console.log("server file is running")
+
+// function add(a,b){
+//     return a + b;
+// }
+// var result =add(2,9)
+// console.log(result)
+
+// const add =(a,b)=>{return a+b;}
+// const result = add(4,5)
+// console.log(result)
+
+// (function(){
+//     console.log("Anand added")
+// })();
+// function callback(){
+//     console.log("Anand is calling is callback function")
+// }
+// const add = function(a,b, callback){
+//     var result = a + b;
+//     console.log("result:"+result) 
+//     callback();
+// }
+// add(4,5, callback)
+
+
