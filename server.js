49f848fa0c138b6df66a942412224a0af35be0b2
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-10-23 06:01:10 +0530

    server create

--- server.js
+++ server.js
@@ -23,6 +23,50 @@
 // }
 // add(4,5, callback)
 
+// var fs = require("fs")
+// var os = require('os')
+
+// var user = os.userInfo();
+// console.log(user.username)
+
+
+// fs.appendFile('greeting.txt','Hi' + user.username + '!\n',()=>{
+//     console.log("file is created")
+// })
+
+// console.log(os)
+// console.log(fs)
+
+// const notes = require('./notes.js')
+// console.log("node file is loaded")
+// var _ = require('lodash')
+
+// var age = notes.age;
+// console.log(age)
+
+// var result = notes.addNumber(age+15,10)
+// console.log("result is now " + result)
+
+// var data = ["person","person","1","2","1","2","age"]
+// var filter = _.uniq(data)
+// console.log(filter);
+
+// const jsonString = '{"name": "Anand Kumar Gautam", "age": 23, "city": "Varanasi", "skills": ["React.js", "Next.js", "Tailwind CSS"]}';
+// const jsonObject = JSON.parse(jsonString);
+
+// console.log(jsonObject);
+
+const jsonObject = {
+    name: "Anand Kumar Gautam",
+    age: 23,
+    city: "Varanasi",
+    skills: ["React.js", "Next.js", "Tailwind CSS"]
+  };
+  const jsonString = JSON.stringify(jsonObject);
+
+console.log(jsonString);
+
+
 
 
 
