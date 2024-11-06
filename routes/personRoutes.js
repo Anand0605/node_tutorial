ebf3f73d6195f859719e3ae3c661f57bc7fe2672
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-11-07 00:00:34 +0530

    update crud operation

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -48,4 +48,49 @@ router.get('/', async (req, res) => {
     }
   })
 
+  router.put('/:id', async (req, res) => {
+    try {
+      const personId = req.params.id; // Extract the id from the URL parameter
+      const updatedPersonData = req.body; // Updated data for the person
+  
+      const response = await Person.findByIdAndUpdate(personId, updatedPersonData, {
+        new: true, // Return the updated document
+        runValidators: true, // Run Mongoose validation
+      });
+
+      if (!response) {
+        return res.status(404).json({ error: 'Person not found' });
+      }
+      
+  
+      console.log('data updated');
+      res.status(200).json(response);
+    } catch (err) {
+      console.error('Error updating data:', err);
+      res.status(500).json({ message: 'Failed to update data', error: err.message });
+    }
+  });
+
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const personId = req.params.id; // Extract the person's ID from the URL parameter
+  
+      // Use findByIdAndDelete instead of findByIdAndRemove
+      const response = await Person.findByIdAndDelete(personId);
+  
+      if (!response) {
+        return res.status(404).json({ error: 'Person not found' });
+      }
+  
+      console.log('data deleted');
+      res.status(200).json({ message: 'Person Deleted Successfully' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  
+
   module.exports = router;
